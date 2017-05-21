c9a103cc354037b0d813a1039429559e69b4c5cb
Sebastián Ariel Szperling <sebastian.szperling@despegar.com>
2017-05-21 16:36:31 -0300

    Agregar input para problema 3

--- src/problema3.cpp
+++ src/problema3.cpp
@@ -1,7 +1,51 @@
-#include <stdio.h>
-#include "Utils.h"
+#include <vector>
+#include <iostream>
+#include "graph.h"
+#include "reconfiguration.h"
+
+struct input {
+    int n;
+    std::vector<edge> existing;
+    std::vector<edge> potential;
+};
 
 int main() {
-    Utils::log(ERROR, "¡Este problema no esta implementado! ¡No me entreguen!");
+    bool hasInput = true;
+    std::vector<input> inputs;
+    while(hasInput) {
+        int n;
+
+        std::cin >> n;
+        hasInput = n != -1;
+        if(hasInput) {
+            input in;
+            in.n = n;
+            for(int i = 0; i < ((n * (n-1)) / 2); ++i) {
+                int c1, c2, e, p;
+                std::cin >> c1 >> c2 >> e >> p;
+                --c1;
+                --c2;
+                if(e) {
+                    in.existing.push_back({c1, c2, p});
+                } else {
+                    in.potential.push_back({c1, c2, p});
+                }
+            }
+            inputs.push_back(in);
+        }
+    }
+    std::vector<input>::const_iterator it;
+    for(it = inputs.begin(); it != inputs.end(); ++it) {
+        int p;
+        std::set<edge> edges = rebuildRoads(it->n, p, it->existing, it->potential);
+
+        printf("%d %lu", p, edges.size());
+
+        std::set<edge>::const_iterator e;
+        for (e = edges.begin(); e != edges.end(); ++e) {
+            printf(" %d %d", e->start + 1, e->end +1);
+        }
+        printf("\n");
+    }
     return 0;
 }

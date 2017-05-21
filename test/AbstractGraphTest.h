57318df86b2c53b2bad8e6bd5a7eb80df54f48a8
Roni <ronibarylko@hotmail.com>
2017-05-21 20:37:12 -0300

    test delivery

--- test/AbstractGraphTest.h
+++ test/AbstractGraphTest.h
@@ -3,12 +3,41 @@
 
 class AbstractGraphTest : public ::testing::Test {
 protected:
-    // TODO: RONI AGREGA GRAFOS ACA
-    edgeList firstGraph;
-    edgeList secondGraph;
-    edgeList thirdGraph;
+    edgeList firstGraph; // Grafo que aparece de ejemplo para Dijkstra en la teórica.
+        // n = 6
+    edgeList secondGraph; // Grafo que aparece de ejemplo para Ford en la teórica. WARN: ARISTAS NEGATIVAS.
+        // n = 6
+    edgeList thirdGraph; // Grafo que aparece de ejemplo para Floyd en la teórica. WARN: ARISTAS NEGATIVAS
+        // n = 4
 
     virtual void SetUp() {
-        // TODO: SET UP DE GRAFOS ACA
+        firstGraph.push_back({0,1,4});
+        firstGraph.push_back({1,2,3});
+        firstGraph.push_back({2,3,1});
+        firstGraph.push_back({4,3,4});
+        firstGraph.push_back({5,4,3});
+        firstGraph.push_back({0,5,3});
+        firstGraph.push_back({1,4,1});
+        firstGraph.push_back({0,2,7});
+
+        secondGraph.push_back({0,1,4});
+        secondGraph.push_back({1,2,3});
+        secondGraph.push_back({2,3,1});
+        secondGraph.push_back({4,3,4});
+        secondGraph.push_back({5,4,3});
+        secondGraph.push_back({0,5,3});
+        secondGraph.push_back({1,4,1});
+        secondGraph.push_back({0,2,7});
+        secondGraph.push_back({1,5,-2});
+
+        thirdGraph.push_back({0,1,3});
+        thirdGraph.push_back({1,0,2});
+        thirdGraph.push_back({1,3,2});
+        thirdGraph.push_back({3,1,4});
+        thirdGraph.push_back({2,3,1});
+        thirdGraph.push_back({3,2,4});
+        thirdGraph.push_back({2,1,-2});
+        thirdGraph.push_back({0,3,3});
+        thirdGraph.push_back({1,2,2});
     }
 };
--- test/DeliveryTest.cpp
+++ test/DeliveryTest.cpp
@@ -1,5 +1,6 @@
 #include "AbstractGraphTest.h"
 #include "../src/delivery.h"
+#include "../src/Utils.h"
 
 class DeliveryTest : public AbstractGraphTest {
 protected:
@@ -23,4 +24,40 @@ TEST_F(DeliveryTest, fewEdges) {
     addEdgeToPremiumGraph(2, 1, 1, 3, k, n, edges);
 
     ASSERT_EQ(optimumDelivery(origin, destination, n, k, edges), 9);
+}
+
+TEST_F(DeliveryTest, straightPath) {
+    n = 11;
+    origin = 0;
+    destination = 10;
+
+    for (int i = 0; i < 9; i++) {
+        edges.clear();
+        k = i;
+        addEdgeToPremiumGraph(0, 1, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(1, 2, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(2, 3, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(3, 4, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(4, 5, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(5, 6, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(6, 7, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(7, 8, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(8, 9, 0, 20, k, n, edges);
+        addEdgeToPremiumGraph(9, 10, 0, 20, k, n, edges); //Camino de 0 a 10, derecho y sin rutas premium
+
+        addEdgeToPremiumGraph(0, 2, 1, 20, k, n, edges);
+        addEdgeToPremiumGraph(2, 4, 1, 20, k, n, edges);
+        addEdgeToPremiumGraph(4, 6, 1, 20, k, n, edges);
+        addEdgeToPremiumGraph(6, 8, 1, 20, k, n, edges);
+        addEdgeToPremiumGraph(8, 10, 1, 20, k, n, edges); //Rutas premium que reducen el camino. El mejor camino posible es usando todas.
+
+        int answerWithK;
+        if (i <=5){
+            answerWithK = 20*10 - 20*i; // Por cada ruta premium se ahorra una ruta de peso 20.
+        } else {
+            answerWithK = 20*10 - 20*5; // Solo hay 5 rutas premium.
+        }
+
+        ASSERT_EQ(optimumDelivery(origin, destination, n, k, edges), answerWithK);
+    }
 }

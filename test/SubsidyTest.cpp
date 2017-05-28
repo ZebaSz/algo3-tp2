0b2cc352adf40f918abf7899e7640779505e8c16
CatOfTheCannals <carlosr.giudice@gmail.com>
2017-05-27 23:38:06 -0300

    en shortestPathTest agregue un test que sirve para ver que dependiendo del source, se detecta o no se detecta ciclo negativo. en subsidyTest agregue un test que muestra que hay grafos conexos que no son fuertemente conexos, para los cuales no se puede acceder a todos los ciclos desde un solo nodo

--- test/SubsidyTest.cpp
+++ test/SubsidyTest.cpp
@@ -56,3 +56,45 @@ TEST_F(SubsidyTest, twoComps) {
     edges.push_back({5,8,7});
     ASSERT_EQ(binarySearchTax(9, 10, edges), 5);
  }
+
+TEST_F(SubsidyTest, noUniversalParents) {
+
+    //las aristas de peso = 5 pertenecen al k3 formado por 0,1,2
+    //las aristas de peso = 10 pertenecen al k3 formado por 4,5,6
+    //las aristas de peso = 15 pertenecen al k3 formado por 7,8,9
+    //las aristas de peso = 20 no pertenecen a ciclos
+    edges.push_back({0,1,5});
+    edges.push_back({1,2,5});
+    edges.push_back({2,0,5});
+    edges.push_back({2,3,20});
+    edges.push_back({4,5,10});
+    edges.push_back({5,6,10});
+    edges.push_back({6,4,10});
+    edges.push_back({6,3,20});
+    edges.push_back({3,7,20});
+    edges.push_back({7,8,15});
+    edges.push_back({8,9,15});
+    edges.push_back({9,7,15});
+    ASSERT_EQ(binarySearchTax(10, 12, edges), 5);
+
+    edges.clear();
+
+    //ahora el k3 1,2,3 tiene pesos iguales a 10 y el k3 4,5,6 tiene pesos iguales a 5
+    //la idea es que este es un grafo conexo, pero no fuertemente conexo
+    //el desafio es detectar todos los ciclos diferentes de una componente conexa
+    //aun cuando no se puede acceder a ellos desde un solo source
+    edges.push_back({0,1,10});
+    edges.push_back({1,2,10});
+    edges.push_back({2,0,10});
+    edges.push_back({2,3,20});
+    edges.push_back({4,5,5});
+    edges.push_back({5,6,5});
+    edges.push_back({6,4,5});
+    edges.push_back({6,3,20});
+    edges.push_back({3,7,20});
+    edges.push_back({7,8,15});
+    edges.push_back({8,9,15});
+    edges.push_back({9,7,15});
+    ASSERT_EQ(binarySearchTax(10, 12, edges), 5);
+
+}

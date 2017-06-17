f23e546c09aea465508e08e3aeb35c0410494df3
Ignacio Tarrio <itarrio@despegar.com>
2017-06-17 16:10:26 -0300

    Comentarios en Fibo

--- src/shortest.cpp
+++ src/shortest.cpp
@@ -96,18 +96,7 @@ bool bellmanFordWithAdjustment(int n, int c, int *distance, const edgeList &edge
     return res;
 }
 
-/**
- * A generic implementation of Dijkstra shortest path algorithm for regular graph and digraph
- *
- * @param source the source node
- * @param n the number of nodes
- * @param m the number of edges
- * @param distance the list of distances to output
- * @param edges the list of edges
- * @param digraph true if it is a digraph
- *
- */
-
+// Comparison function for making the boost heap a min heap, boost.heap implements priority queues as max-heaps to be consistent with the STL heap functions.
 struct compareMin {
     bool operator()(const adjacency& n1, const adjacency& n2) const {
         return n1.weight > n2.weight;
@@ -118,8 +107,22 @@ typedef boost::heap::fibonacci_heap<adjacency, boost::heap::compare<compareMin>>
 
 typedef minQueue::handle_type queueNode;
 
+/**
+ * A implementation of Dijkstra shortest path algorithm for graphs, with Fibonacci Heaps and Adjency Lists
+ *
+ * @param source the source node
+ * @param n the number of nodes
+ * @param m the number of edges
+ * @param distance the list of distances to output
+ * @param edges the list of edges
+ *
+ * Running Time: O(|E|+|V|*log(|V|))
+ *
+ */
+
 void dijkstra(int source, int n, int *distance, const edgeList &edges) {
 
+    //Create adjacency list
     std::list<adjacency>* adjacencyList = new std::list<adjacency>[n];
     for(edgeList::const_iterator it = edges.begin(); it != edges.end(); ++it) {
         adjacencyList[it->start].push_back({it->end, it->weight});
@@ -127,30 +130,31 @@ void dijkstra(int source, int n, int *distance, const edgeList &edges) {
 
     distance[source] = 0;
 
+    //Initialize distance and priority queue
     minQueue queue;
     queueNode* nodesOnQueue = new queueNode[n];
-
     for (int i = 0; i < n; i++){
         if (i != source) {
             distance[i] = INF;
         }
         nodesOnQueue[i] = queue.push({i, distance[i]});
     }
-    
+
     while (!queue.empty()) {
+        //Get and remove the best vertex from the queue
         adjacency min = queue.top();
         queue.pop();
         for (std::list<adjacency>::iterator it = adjacencyList[min.node].begin(); it != adjacencyList[min.node].end(); ++it) {
-            if (distance[min.node] != INF) {
+            if (distance[min.node] != INF) { //Compare with infinity because if we sum INF with a positive number we get a negative weight
                 int dist = distance[min.node] + it->weight;
                 if (dist < distance[it->node]) {
                     distance[it->node] = dist;
-                    queue.increase(nodesOnQueue[it->node], {it->node, dist});
+                    queue.increase(nodesOnQueue[it->node], {it->node, dist}); //Update the queue
                 }
             }
         }
     }
-
+    //Remove from memory auxiliary data
     delete[] adjacencyList;
     delete[] nodesOnQueue;
  }
